98879b078b349a9afe16104afeb17d3e519f7c93
Toutatis <search@Toutatiss-Mac-mini.local>
2019-08-08 12:22:53 -0700

    jwt-extended

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+
+from resources.user import UserRegister, User, UserLogin
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'  #app.config['JWT_SECRET_KEY']
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+	db.create_all()
+
+jwt = JWTManager(app)  # not creating /auth  
+
+
+#api.add_resource(Student, '/student/<string:name>')  # http://127.0.0.1:5000/student/Rolf
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')  
+api.add_resource(StoreList, '/stores')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
+
+if __name__ == '__main__':
+	from db import db
+	db.init_app(app)
+	app.run(port = 5000, debug = True)
+
+
